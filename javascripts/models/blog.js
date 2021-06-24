c873978854e3ad0ab6344dbb94d3c6aa67e35dbf
Enoch2k2 <enoch2k2@hotmail.com>
2021-06-24 10:41:59 -0500

    added delete to blogs

--- javascripts/models/blog.js
+++ javascripts/models/blog.js
@@ -9,20 +9,40 @@ class Blog {
     Blog.all.push(this)
   }
 
+  deleteBlog = () => {
+    fetch('http://localhost:3000/blogs/' + this.id, {
+      method: 'delete',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      Blog.all = Blog.all.filter(blog => blog.id !== this.id);
+      Blog.displayAll();
+    })
+  }
+
   display = () => {
     const div = document.createElement('div');
     const h4 = document.createElement('h4');
     const pAuthor = document.createElement('p');
     const pContent = document.createElement('p');
     const hr = document.createElement('hr');
+    const deleteButton = document.createElement('button');
   
+    deleteButton.innerText = 'Delete';
     h4.innerText = this.title;
     pAuthor.innerText = `By: ${ this.author }`;
     pContent.innerText = this.content;
+
+    deleteButton.addEventListener('click', this.deleteBlog);
   
     div.appendChild(h4);
     div.appendChild(pAuthor);
     div.appendChild(pContent);
+    div.appendChild(deleteButton);
   
     blogsDiv().appendChild(div);
     blogsDiv().appendChild(hr);
@@ -39,5 +59,10 @@ class Blog {
   
     Api.postBlog(blog)
   
+  }
+
+  static displayAll = () => {
+    blogsDiv().innerHTML = '';
+    Blog.all.forEach(blog => blog.display());
   }
 }
